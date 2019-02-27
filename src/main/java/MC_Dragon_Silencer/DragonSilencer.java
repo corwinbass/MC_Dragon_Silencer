3d038e3ba13369b1167521d4c5d714eaa28da169
Corwin Bass <cb454480@gmail.com>
2019-02-27 10:32:56 +0100

    Gradleize project

---
+++ src/main/java/MC_Dragon_Silencer/DragonSilencer.java
@@ -0,0 +1,45 @@
+package DragonSilencer;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DragonSilencer extends JavaPlugin {
+
+    private PacketAdapter Adapter = null;
+
+    @Override
+    public void onEnable() {
+        try {
+            this.Adapter = new PacketAdapter(this, ListenerPriority.NORMAL,
+                    PacketType.Play.Server.WORLD_EVENT) {
+                public void onPacketSending(PacketEvent event) {
+                    if (event.getPacketType() == PacketType.Play.Server.WORLD_EVENT) {
+                        if (event.getPacket().getIntegers().read(0) == 1028) {
+                            event.setCancelled(true);
+                        }
+                    }
+                }
+            };
+
+            ProtocolLibrary.getProtocolManager().addPacketListener(this.Adapter);
+            getLogger().info("DragonSilencer successfully enabled.");
+        } catch(Exception ex) {
+            getLogger().info("DragonSilencer - error occurred during start up.");
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            ProtocolLibrary.getProtocolManager().removePacketListener(this.Adapter);
+            this.Adapter = null;
+            getLogger().info("DragonSilencer successfully disabled.");
+        } catch(Exception ex) {
+            getLogger().info("DragonSilencer - error occurred during disabling plugin.");
+        }
+    }
+}
